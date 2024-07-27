ad95e6ff47751e0f1bd2d20af07e3e45506426e1
Jacob Jørgensen <111784303+Zelvios@users.noreply.github.com>
2024-07-28 00:44:51 +0200

    Removed Head to add it to index under components.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,11 @@
 import { Inter } from "next/font/google";
 import { HomPage } from "@/components";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>Jacob-J</title>
-                <meta name="description"  content="A portfolio created to showcase expertise in web development, design, and programming." />
-            </Head>
             <main className={inter.className}>
                 <HomPage />
             </main>
