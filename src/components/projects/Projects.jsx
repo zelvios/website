63110b14b4973bc7fa7d5954ee9f2338e10bfa19
Jacob Jørgensen <me@jacob-j.com>
2024-09-26 12:11:01 +0200

    updated projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -18,53 +18,18 @@ export const Projects = () => {
 
 const projects = [
     {
-        title: "Rust Api",
-        imgSrc: "project-imgs/example-project.jpg",
-        code: "https://www.github.com",
-        projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        tech: ["Rust", "C#", "Python", "FastAPI"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        modalContent: (
-            <>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>Fusce vehicula justo ut lorem fermentum, vel scelerisque mauris feugiat.</p>
-                <p>Curabitur eu nisi id elit dignissim interdum.</p>
-                <p>Proin ut bibendum ante. Nullam imperdiet risus nec orci ultrices, at aliquet lacus consectetur.</p>
-                <p>Nam et turpis sed nisl malesuada consectetur eget at augue.</p>
-            </>
-        ),
-    },
-    {
-        title: "Eat More",
-        imgSrc: "project-imgs/example-project.jpg",
-        code: "https://www.github.com",
-        projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        tech: ["Node", "Express", "Postgres", "Kafka", "Redis"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        modalContent: (
-            <>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>Phasellus ultricies metus id libero convallis, vel dictum dolor laoreet.</p>
-                <p>Nullam auctor urna non velit condimentum, ut facilisis ante cursus.</p>
-                <p>Suspendisse id velit ut justo varius consequat ac ut nisi.</p>
-                <p>Aenean sed magna ut lorem posuere cursus et a erat.</p>
-            </>
-        ),
-    },
-    {
-        title: "Eat Less",
-        imgSrc: "project-imgs/example-project.jpg",
-        code: "https://www.github.com",
+        title: "Todo-TUI",
+        imgSrc: "project-imgs/todo-tui.png",
+        code: "https://github.com/Zelvios/website",
         projectLink: "",
-        tech: ["NextJS", "Java", "Spring", "AWS Aurora"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        tech: ["Rust"],
+        description: "A simple Todo app designed with ratatui.rs",
         modalContent: (
             <>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>Donec ut diam nec velit tincidunt cursus sit amet in lectus.</p>
-                <p>Vestibulum vel dolor nec libero convallis feugiat.</p>
-                <p>Nam ac nisi et arcu placerat feugiat.</p>
-                <p>Integer convallis orci nec arcu condimentum, nec lacinia erat dictum.</p>
+                <p>A simple todo app made in rust.</p>
+                <p>You are able to create, edit and delete todos.</p>
+                <p>The app also adds a progress system to your todos: "Waiting", "In Progress" and "Done".</p>
+                <p>There is also support for color change that changes the color throughout app.</p>
             </>
         ),
     },
