22d426cf6ea165e1826b68d5f79715c425d90689
Jacob Jørgensen <me@jacob-j.com>
2025-02-07 07:50:45 +0100

    style: add fade in animation for header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,10 +5,10 @@
     }
 </script>
 
-<header class="bg-base bg-opacity-40 backdrop-blur-md text-text p-6 shadow-md flex flex-col items-center justify-between h-[15vh] fixed bottom-0 left-0 right-0 z-10 border-t-2 border-accent">
+<header class="bg-base bg-opacity-40 backdrop-blur-md text-text p-6 shadow-md flex flex-col items-center justify-between h-[15vh] fixed bottom-0 left-0 right-0 z-10 border-t-2 border-accent fade-in-left">
     <nav class="mt-4 text-center">
         <ul class="flex justify-center gap-6 flex-wrap">
-            <li>
+            <li class="nav-item">
                 <a
                         href="/"
                         class="text-base p-2 rounded"
@@ -19,7 +19,7 @@
                     Home
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a
                         href="/projects"
                         class="text-base p-2 rounded"
@@ -30,7 +30,7 @@
                     Projects
                 </a>
             </li>
-            <li>
+            <li class="nav-item">
                 <a
                         href="/contact"
                         class="text-base p-2 rounded"
@@ -44,3 +44,57 @@
         </ul>
     </nav>
 </header>
+
+<style>
+    .fade-in-left {
+        animation: fadeInLeft 1s ease-out;
+    }
+
+    @keyframes fadeInLeft {
+        0% {
+            opacity: 0;
+            transform: translateX(-100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+
+    @keyframes fadeInBorder {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .nav-item {
+        opacity: 0;
+        animation: fadeInItem 1s ease-out forwards;
+    }
+
+    .nav-item:nth-child(1) {
+        animation-delay: 1s;
+    }
+
+    .nav-item:nth-child(2) {
+        animation-delay: 1.2s;
+    }
+
+    .nav-item:nth-child(3) {
+        animation-delay: 1.4s;
+    }
+
+    @keyframes fadeInItem {
+        0% {
+            opacity: 0;
+            transform: translateX(-20px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+</style>
