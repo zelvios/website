2a76d2ac17194a795104b7ca1ce577c4ec5256e7
Jacob Jørgensen <me@jacob-j.com>
2025-04-06 01:02:42 +0200

    fix: edit background to work on screens with smaller width.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
-<main class="text-text">
-    <!-- ===== Hero Section Start ===== -->
-    <section class="relative overflow-hidden z-10 pt-35 md:pt-25 xl:pt-45">
-        <div class="mx-auto">
-            <div class="absolute -z-10 pointer-events-none inset-0 overflow-hidden">
+<main>
+    <section class="relative overflow-hidden z-10 pt-35 md:pt-40 xl:pt-20">
+        <div class="max-w-7xl mx-auto">
+            <div class="absolute -z-10 pointer-events-none inset-0 overflow-hidden -mx-28">
                 <div class="absolute top-0 left-1/2 -translate-x-1/2 -u-z-10">
                     <img alt="blur" class="max-w-none" src="./images/blur/blur-02.svg">
                 </div>
@@ -30,87 +29,87 @@
                 </div>
 
                 <a href="/projects">
-                <button>
-                    ↪ Projects
-                    <div class="star-1">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                    <button>
+                        ↪ Projects
+                        <div class="star-1">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                    <div class="star-2">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                        </div>
+                        <div class="star-2">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                    <div class="star-3">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                        </div>
+                        <div class="star-3">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                    <div class="star-4">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                        </div>
+                        <div class="star-4">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                    <div class="star-5">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                        </div>
+                        <div class="star-5">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                    <div class="star-6">
-                        <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
-                             viewBox="0 0 784.11 815.53"
-                             xml:space="preserve"
-                             xmlns="http://www.w3.org/2000/svg">
+                        </div>
+                        <div class="star-6">
+                            <svg style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                                 viewBox="0 0 784.11 815.53"
+                                 xml:space="preserve"
+                                 xmlns="http://www.w3.org/2000/svg">
       <defs></defs>
-                            <g id="Layer_x0020_1">
+                                <g id="Layer_x0020_1">
         <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                                <path class="fil0"
-                                      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
+                                    <path class="fil0"
+                                          d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"></path>
       </g>
     </svg>
-                    </div>
-                </button>
+                        </div>
+                    </button>
                 </a>
             </div>
 
@@ -133,28 +132,30 @@
 
                 <!-- All Skills -->
                 <div class="relative flex justify-center mb-6">
-                    <input type="checkbox" id="toggle-tooltip" class="hidden">
+                    <input class="hidden" id="toggle-tooltip" type="checkbox">
 
-                    <label for="toggle-tooltip" class="relative px-6 py-3 text-sm font-semibold text-text rounded-xl focus:outline-none">
+                    <label class="relative px-6 py-3 text-sm font-semibold text-text rounded-xl focus:outline-none"
+                           for="toggle-tooltip">
         <span class="relative flex items-center gap-2">
-            <svg viewBox="0 0 24 24" stroke="currentColor" fill="none" class="w-4 h-4">
+            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path
                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
             </svg>
             Click For All Skills
         </span>
                     </label>
 
                     <div
-                            class="absolute hidden opacity-0 bottom-full left-1/2 -translate-x-1/2 mb-3 w-100 h-90 transition-all duration-300 ease-out transform"
+                            class="absolute opacity-0 bottom-full left-1/2 -translate-x-1/2 mb-3 w-100 h-90 transition-all duration-300 ease-out transform pointer-events-none"
                             id="tooltip"
                     >
                         <div class="relative p-4 bg-gradient-to-br from-gray-900/95 to-gray-800/95 backdrop-blur-md rounded-2xl border border-white/10 shadow-[0_0_30px_rgba(79,70,229,0.15)]">
                             <div class="flex items-center gap-3 mb-2">
                                 <div class="flex items-center justify-center w-8 h-8 rounded-full bg-indigo-500/20">
-                                    <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 text-indigo-400">
-                                        <path d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" fill-rule="evenodd"></path>
+                                    <svg class="w-4 h-4 text-indigo-400" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                              fill-rule="evenodd"></path>
                                     </svg>
                                 </div>
                                 <h3 class="text-sm font-semibold text-text">Experience With</h3>
@@ -162,40 +163,29 @@
 
                             <!-- Skill Categories -->
                             <div class="space-y-4">
-                                <!-- Code Section -->
                                 <div class="space-y-2">
                                     <h4 class="font-semibold text-sm text-text">Code ➤</h4>
-                                    <p class="text-sm text-text">
-                                        Rust, C, C#, Python, JavaScript, TypeScript, HTML, CSS
-                                    </p>
+                                    <p class="text-sm text-text">Rust, C, C#, Python, JavaScript, TypeScript, HTML,
+                                        CSS</p>
                                 </div>
 
-                                <!-- Framework Section -->
                                 <div class="space-y-2">
                                     <h4 class="font-semibold text-sm text-text">Framework ➤</h4>
-                                    <p class="text-sm text-text">
-                                        Vue, Bootstrap, Dotnet
-                                    </p>
+                                    <p class="text-sm text-text">Vue, Bootstrap, Dotnet</p>
                                 </div>
 
-                                <!-- Database Section -->
                                 <div class="space-y-2">
                                     <h4 class="font-semibold text-sm text-text">Database ➤</h4>
-                                    <p class="text-sm text-text">
-                                        SqLite, PostgreSQL, MongoDB
-                                    </p>
+                                    <p class="text-sm text-text">SqLite, PostgreSQL, MongoDB</p>
                                 </div>
 
-                                <!-- Tools / Syustems Section -->
                                 <div class="space-y-2">
                                     <h4 class="font-semibold text-sm text-text">Tools / Systems ➤</h4>
-                                    <p class="text-sm text-text">
-                                        Git, Docker, Linux
-                                    </p>
+                                    <p class="text-sm text-text">Git, Docker, Linux</p>
                                 </div>
 
                                 <div class="flex items-center gap-2 text-xs text-gray-400">
-                                    <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                         <path
                                                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                                                 fill-rule="evenodd"></path>
@@ -203,18 +193,14 @@
                                     <span>See examples under Projects</span>
                                 </div>
                             </div>
-
-                            <div class="absolute inset-0 rounded-2xl bg-gradient-to-r from-indigo-500/10 to-purple-500/10 blur-xl opacity-50"></div>
-
-                            <div class="absolute -bottom-1.5 left-1/2 -translate-x-1/2 w-3 h-3 bg-gradient-to-br from-gray-900/95 to-gray-800/95 rotate-45 border-r border-b border-white/10"></div>
                         </div>
                     </div>
                 </div>
-
     </section>
 </main>
 
 <style>
+
     #toggle-tooltip:checked ~ #tooltip {
         display: block;
         opacity: 1;
@@ -478,23 +464,6 @@
         margin: 5px;
     }
 
-    @media (max-width: 768px) {
-        .skills-list {
-            justify-content: center;
-        }
-
-        .skills-list span {
-            font-size: 14px;
-        }
-    }
-
-    @media (max-width: 480px) {
-        .skills-list span {
-            font-size: 12px;
-            padding: 4px 8px;
-        }
-    }
-
     .skills-list span:nth-child(1) {
         animation-delay: 0s;
     }
@@ -527,6 +496,41 @@
         100% {
             transform: translateY(-20px);
             opacity: 0;
+        }
+    }
+
+    @media (max-width: 1000px) {
+        .skills-list {
+            display: flex;
+            justify-content: center;
+            flex-wrap: nowrap;
+            gap: 10px;
+            width: 100%;
+        }
+
+        .skills-list span {
+            font-size: 12px;
+        }
+
+        .card {
+            width: 100%;
+            height: 100px;
+            padding: 15px 0;
+            box-sizing: border-box;
+        }
+    }
+
+
+    @media (max-width: 480px) {
+        .skills-list span {
+            font-size: 14px;
+            padding: 4px 8px;
+        }
+
+        .card {
+            width: 90%;
+            height: 90px;
+            padding: 10px;
         }
     }
 </style>
